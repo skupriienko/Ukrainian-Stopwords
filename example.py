6cf5a97c75b05855381399379345778b32771e6e
skupriienko <61395455+skupriienko@users.noreply.github.com>
2020-09-30 11:52:57 +0300

    an example for python and pandas dataframes

---
+++ example.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+stopwords_ua = pd.read_csv("stopwords_ua.txt", header=None, names=['stopwords'])
+stop_words_ua = list(stopwords_ua.stopwords)
+
+def clean_text(text):
+    text = "".join([word for word in text if word not in string.punctuation])
+    tokens = re.split('\W+', text)
+    text = [word for word in tokens if word not in stop_words_ua]
+    return text
+    
+# create your dataframe with a text column
+data = pd.DataFrame(articles_text_list, columns=['article_title', 'txt', 'article_source_url', 'url_of_article', 'article_keywords', 'article_publish_date'])
+data.sort_values(by='url_of_article') 
+
+# data["txt"] is your text column
+
+# remove stopwords
+data['body_text_nostop'] = data["txt"].apply(lambda x: clean_text(x.lower()))
