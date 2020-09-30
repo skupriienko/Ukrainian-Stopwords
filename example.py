e5a0f3d731754c5a7b6fd0c57477e0e1c3e4eb3b
skupriienko <61395455+skupriienko@users.noreply.github.com>
2020-09-30 21:06:07 +0300

    add comments

--- example.py
+++ example.py
@@ -8,12 +8,16 @@ def clean_text(text):
     tokens = re.split('\W+', text)
     text = [word for word in tokens if word not in stop_words_ua]
     return text
-    
-# create your dataframe with a text column
+
+
+# articles_text_list is your list with text
+
+# create your dataframe with a text column like this one from your list
 data = pd.DataFrame(articles_text_list, columns=['article_title', 'txt', 'article_source_url', 'url_of_article', 'article_keywords', 'article_publish_date'])
 data.sort_values(by='url_of_article') 
 
 # data["txt"] is your text column
 
 # remove stopwords
+# try it after preprocessing text
 data['body_text_nostop'] = data["txt"].apply(lambda x: clean_text(x.lower()))
